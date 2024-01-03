fa7c7f0d558e0ac1e75ee4652baa26ce8b9ce689
Ndifoinhilar@gmail.com <ndifoinhilary@gmail.com>
2024-01-03 08:34:00 +0100

    Added forms for creating the user

---
+++ auth/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from auth.models import User
+
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(min_length=6, widget=forms.PasswordInput)
+    comfirm_password = forms.CharField(min_length=6, widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ["email", "first_name", "password", "comfirm_password"]
+
+    def clean_comfirm_password(self):
+        password = self.cleaned_data.get("password")
+        comfirm_password = self.cleaned_data.get("comfirm_password")
+
+        if password != comfirm_password:
+            raise forms.ValidationError("Password does not match")
+        return password
+
+    def save(self, *args, **kwargs):
+        user = super().save(commit=False)
+        password = self.cleaned_data.get("password")
+        user.set_password(password)
+        user.save()
+        return user
